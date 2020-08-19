239c0553512d96349467291ff919290de62e143b
Bebjamin <69919870+Bebjamin@users.noreply.github.com>
2020-08-19 14:12:46 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/************************************************************
+* Hello.java
+*************************************************************
+* Benjamin Yiu
+* 08/19/2020
+* CMSC 255 002
+**************************************************************/
+
+public class Hello {
+	
+	public static void main(string[] args) {
+
+	System.out.printin("<Benjamin Yiu>");
+	System.out.printin("<Lab 1>");
+	System.out.printin("<First lab in cmsc>");
+	System.out.printin("<08/19/2020>");
+	System.out.printin("<Cmsc255 002>");
+	System.out.printin();
+
+	}
+}
